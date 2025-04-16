aeb3733ee8b4f93c0dfaa78b3a48b0b4918448e1
Peter Manchester <64981120+petermanchester1987@users.noreply.github.com>
2025-04-16 14:16:05 +0100

    feat: start root layout and constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "The Upbeats Duo Store";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern ecommerce platform built with NextJS for The Upbeats Duo Store";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
