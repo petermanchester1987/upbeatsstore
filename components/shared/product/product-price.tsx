4c9444b2cade2333dc04569e137f1d7125c1b371
Peter Manchester <64981120+petermanchester1987@users.noreply.github.com>
2025-04-20 18:45:48 +0100

    feat: small price component

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,17 @@
+import {cn} from "@/lib/utils";
+
+const ProductPrice = ({ value, className}: {value: number; className?: string}) => {
+    //ensure two decimal places
+    const stringValue = value.toFixed(2);
+    //get the int/float parts
+    const [intValue, floatValue] = stringValue.split(".");
+    return ( 
+        <p className={ cn("text-2xl", className)}>
+            <span className="text-xs align-super">£</span>
+            {intValue}
+            <span className="text-xs align-super">.{floatValue}</span>
+        </p>
+     );
+}
+ 
+export default ProductPrice;
