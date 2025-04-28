76797aa0de3a0366f401e7af74a8b1b74a00b816
Peter Manchester <64981120+petermanchester1987@users.noreply.github.com>
2025-04-28 09:56:41 +0100

    feat: start on product details page

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,82 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+import ProductPrice from "@/components/shared/product/product-price";
+
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import { notFound } from "next/navigation";
+
+const ProductDetailsPage = async ( props : { params : Promise<{slug: string}>}) => {
+    const { slug } = await props.params
+
+    const product = await getProductBySlug(slug);
+    if (!product) {
+        notFound();
+    }
+    return ( 
+
+    <>
+        <section>
+            <div className="grid grid-cols1 md:grid-cols-5">
+                {/* Images column */}
+                <div className="col-span-2">
+                {/* Images component */}
+                </div>
+                {/* Details column */}
+                <div className="col-span-2 p-5">
+                    <div className="flex flex-col gap-6">
+                        <p>{product.brand} {product.category}</p>
+                        <h1 className="h3-bold">{product.name}</h1>
+                        <p>{product.rating} of {product.numReviews} Reviews</p>
+                        <div className="flex flex-col sm:flex-row sm:items-center gap-3"></div>
+                        <ProductPrice className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2" value={Number(product.price)} />
+                    </div>
+                    <div className="mt-10">
+                        <p className="font-semibold">
+                            Description
+                        </p>
+                        <p>{product.description}</p>
+                    </div>
+                </div>
+                {/* Action column */}
+                <div className="">
+                    <Card>
+                        <CardContent className="p-4">
+                            <div className="mb-2 flex justify-between">
+                                <div className="">Price</div>
+                                <div>
+                                    <ProductPrice value={Number(product.price)} />
+                                </div>
+
+                            </div>
+                            <div className="mb-2 flex justify-between">
+                                <div className="">Status</div>
+                            {product.stock > 0 ? (
+                                <Badge variant="outline">
+                                    In Stock
+                                </Badge>
+                            ) : (
+                                <Badge variant="destructive">
+                                    Out of Stock
+                                </Badge>
+                            )}
+                            </div>
+                            {product.stock > 0 && ( 
+                                <div className="flex-center">
+                                    <Button className="w-full">Add To Cart</Button>
+                                </div>)
+                            }
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+            
+            
+        </section>
+    </>
+
+     );
+}
+ 
+export default ProductDetailsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -14,3 +14,13 @@ export async function getLatestProducts() {
     });
     return convertToPlainObject(products);
 }
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+    const product = await prisma.product.findFirst({
+        where: {
+            slug: slug,
+        },
+    });
+    return convertToPlainObject(product);
+}
