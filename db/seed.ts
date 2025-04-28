52128cb4b46f95cc74a262b5236101a08b7518e9
Peter Manchester <64981120+petermanchester1987@users.noreply.github.com>
2025-04-28 07:46:29 +0100

    fix: had to adjust where prisma generated files

---
+++ db/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import sampleData from "./sample-data";
+
+async function main() {
+    const prisma = new PrismaClient();
+    
+    // Clear existing data
+    await prisma.product.deleteMany();
+    
+    
+    // Create sample products
+    await prisma.product.createMany({
+        data: sampleData.products,
+    });
+    
+    console.log("DB seeded successfully.");
+}
+
+main()
